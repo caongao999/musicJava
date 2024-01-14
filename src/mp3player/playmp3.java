f4b931e4402d633d27fa64739f99102c9d0c54d5
Gawasna <hunglepy05@gmail.com>
2024-01-15 01:13:00 +0700

    update
    thêm:
    -lọc file
    -nâng cấp tìm kiếm
    -bỏ viền di chuột , sửa playlist event
    -

--- src/mp3player/playmp3.java
+++ src/mp3player/playmp3.java
@@ -1,25 +1,42 @@
 
 package mp3player;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import mp3player.Login;
+import preDemontease.PlaceholderTextField;
+
 import java.awt.GraphicsConfiguration;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.io.*;
 
+import javax.sound.sampled.FloatControl;
 import javax.swing.AbstractButton;
 import javax.swing.DefaultListModel;
 import java.util.ArrayList;
+import java.util.Comparator;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableModel;
 import data.ConnectData;
+import javazoom.jl.decoder.Bitstream;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.JavaSoundAudioDevice;
+import javazoom.jl.player.Player;
+import javazoom.jl.player.advanced.PlaybackEvent;
+import javazoom.jl.player.advanced.PlaybackListener;
+
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.FocusEvent;
@@ -27,13 +44,22 @@ import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.plaf.metal.MetalSliderUI;
 /**
  *
  * @author ali
  */
 public class playmp3 extends javax.swing.JFrame {
+    private static final int MAX_VOLUME = 150;
+    private static final float BOOST_THRESHOLD = 100.0f;
+    private Timer timer;
+    
     Playlist pl = new Playlist();
     ArrayList updateList = new ArrayList();
     javazoom.jl.player.Player player;
@@ -42,8 +68,7 @@ public class playmp3 extends javax.swing.JFrame {
         initComponents();
        this.setIconImage(new ImageIcon(getClass().getResource("music-icon.png")).getImage());  
         playlistevent();
-       
-//jLabel15 and jLabel16
+        addSearchFieldListener();
     }
      
 void updateList() {
@@ -60,31 +85,67 @@ void updateList() {
 
 //panel kontrol
 
-void add() { //Thêm thông báo trùng lặp
+private File lastChosenDirectory; 
+
+void add() {
     JFileChooser fileChooser = new JFileChooser();
-    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-
+    if (lastChosenDirectory != null) {
+        fileChooser.setCurrentDirectory(lastChosenDirectory);
+    }
+    
+    FileNameExtensionFilter audioFilter = new FileNameExtensionFilter("Audio Files", "mp3", "wav", "ogg");
+    fileChooser.setFileFilter(audioFilter);
     int result = fileChooser.showOpenDialog(this);
-
     if (result == JFileChooser.APPROVE_OPTION) {
         File selectedFile = fileChooser.getSelectedFile();
-
-        if (pl.ls.contains(selectedFile)) {
-            int option = JOptionPane.showConfirmDialog(this, "Bài hát đã tồn tại. Bạn có muốn thay thế nó không?",
-                    "Tệp đã tồn tại", JOptionPane.YES_NO_OPTION);
-
-            if (option == JOptionPane.YES_OPTION) {
-
-                int index = pl.ls.indexOf(selectedFile);
-                pl.ls.set(index, selectedFile);
+        if (isAudioFile(selectedFile)) {
+            lastChosenDirectory = selectedFile.getParentFile();
+            if (pl.ls.contains(selectedFile)) {
+                int option = JOptionPane.showConfirmDialog(this, "Bài hát đã tồn tại. Bạn có muốn thay thế nó không?",
+                        "Tệp đã tồn tại", JOptionPane.YES_NO_OPTION);
+                if (option == JOptionPane.YES_OPTION) {
+                    int index = pl.ls.indexOf(selectedFile);
+                    pl.ls.set(index, selectedFile);
+                }
+            } else {
+                pl.ls.add(selectedFile);
+                updateList();
             }
         } else {
-            pl.ls.add(selectedFile);
+            JOptionPane.showMessageDialog(this, "Định dạng tệp không hợp lệ: " + selectedFile.getName(),
+                    "Lỗi", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
+void addAllFilesInDirectory() {
+    JFileChooser fileChooser = new JFileChooser();
+    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    FileNameExtensionFilter audioFilter = new FileNameExtensionFilter("Audio Files", "mp3", "wav", "ogg");
+    fileChooser.setFileFilter(audioFilter);
+    int result = fileChooser.showOpenDialog(this);
+    if (result == JFileChooser.APPROVE_OPTION) {
+        File selectedDirectory = fileChooser.getSelectedFile();
+        File[] files = selectedDirectory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    if (isAudioFile(file)) {
+                        pl.ls.add(file);
+                    } else {
+                        JOptionPane.showMessageDialog(this, "Định dạng tệp không hợp lệ: " + file.getName(),
+                                "Lỗi", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
             updateList();
         }
     }
 }
 
+private boolean isAudioFile(File file) {
+    String fileName = file.getName().toLowerCase();
+    return fileName.endsWith(".mp3") || fileName.endsWith(".wav") || fileName.endsWith(".ogg");
+}
 
 void remove(){
     try{
@@ -131,39 +192,59 @@ void save(){
 
 File play1;
 static int a = 0;
+private boolean isPlaying = false;
 
-void putar(){
-    if(a==0){
-        try{
-            int p1 = jPlaylist.getSelectedIndex();
-            play1 = (File) this.updateList.get(p1);
+void putar() {
+	  if (!isPlaying) {
+	        try {
+	            int p1;
+	            if (searchfield.getText().isEmpty()) {
+	                p1 = jPlaylist.getSelectedIndex();
+	            } else {
+	                int selectedIndex = jPlaylist.getSelectedIndex();
+	                DefaultListModel model = (DefaultListModel) jPlaylist.getModel();
+	                SearchResult selectedResult = (SearchResult) model.getElementAt(selectedIndex);
+	                p1 = selectedResult.getIndex();
+	            }
+	            play1 = (File) this.updateList.get(p1);;
             FileInputStream fis = new FileInputStream(play1);
             BufferedInputStream bis = new BufferedInputStream(fis);
             player = new javazoom.jl.player.Player(bis);
-            a =1;
-        }catch(Exception e){
+            timer = new Timer(true);
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    progressBar.setValue((int) player.getPosition());
+                    if (player.isComplete()) {
+                        // Bài hát kết thúc, dừng Timer và thiết lập isPlaying thành false
+                        timer.cancel();
+                        isPlaying = false;
+                    }
+                }
+            }, 0, 1000); // Cập nhật mỗi giây
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        player.play();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+            isPlaying = true;
+             }
+             catch (Exception e) {
             System.out.println("Problem playing file");
             System.out.println(e);
         }
-        
-        new Thread(){
-            @Override
-            public void run(){
-                try{
-                    player.play();
-                
-            }catch (Exception e){
-            }
-        }
-    }.start();
-    }else{
+    } else {
+        // Tạm dừng bài hát khi nút được nhấp
         player.close();
-        
-        a=0;
-        putar();
+        timer.cancel(); 
+        isPlaying = false;
     }
 }
-
 
 File sa;
 void next(){
@@ -248,7 +329,10 @@ public void playNextRandom() {
 }
 */
 
-void playlistevent() {//MouseEvent cho Playlist
+private int lastSelectedIndex = -1;
+private boolean isMouseOverList = false;
+
+void playlistevent() {
     jPlaylist.addMouseListener(new MouseAdapter() {
         @Override
         public void mouseClicked(MouseEvent e) {
@@ -259,26 +343,29 @@ void playlistevent() {//MouseEvent cho Playlist
                 }
             }
         }
-    });
 
-    jPlaylist.addMouseMotionListener(new MouseMotionAdapter() {
         @Override
-        public void mouseMoved(MouseEvent e) {
-            int index = jPlaylist.locationToIndex(e.getPoint());
-            jPlaylist.clearSelection(); 
+        public void mouseEntered(MouseEvent e) {
+            isMouseOverList = true;
+        }
 
+        @Override
+        public void mouseExited(MouseEvent e) {
+            isMouseOverList = false;
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            int index = jPlaylist.locationToIndex(e.getPoint());
+            jPlaylist.clearSelection();
             if (index >= 0) {
                 jPlaylist.setSelectedIndex(index);
+            } else {
+                jPlaylist.setSelectedIndex(lastSelectedIndex);
             }
         }
-
-        public void mouseExited(MouseEvent e) {
-            jPlaylist.clearSelection(); 
-        }
     });
-    
 }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -325,6 +412,10 @@ void playlistevent() {//MouseEvent cho Playlist
         OpenInfor = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         OpenMenu = new javax.swing.JLabel();
+        searchfield = new javax.swing.JTextField();
+        shuffleB = new javax.swing.JButton();
+        volumnSlider = new javax.swing.JSlider();
+        progressBar = new javax.swing.JProgressBar();
 
         jButton2.setText("jButton2");
 
@@ -390,23 +481,7 @@ void playlistevent() {//MouseEvent cho Playlist
         jPanel2.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 50, 140, 20));
 
         InforPage.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, 310, 130));
-        searchfield = new PlaceholderTextField("Search");//PlaceHolder
-        searchfield.getDocument().addDocumentListener(new DocumentListener() {//Tính năng tìm Kiếm
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-            	updatePlaylist();
-            }
 
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-            	updatePlaylist();
-            }
-
-            @Override
-            public void changedUpdate(DocumentEvent e) {        
-            }
-        });
-        
         jPanel3.setBackground(new java.awt.Color(204, 204, 255));
         jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -541,27 +616,89 @@ void playlistevent() {//MouseEvent cho Playlist
             }
         });
         getContentPane().add(OpenMenu, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 40, 30));
+
+        searchfield = new PlaceholderTextField("Tìm kiếm");
         getContentPane().add(searchfield, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 30, 540, 40));
+
+        shuffleB.setText("Shuffle");
+        getContentPane().add(shuffleB, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 370, -1, -1));
+        getContentPane().add(volumnSlider, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 470, -1, -1));
+        getContentPane().add(progressBar, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 460, 400, -1));
 
         setSize(new java.awt.Dimension(1075, 563));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
     
-    private void updatePlaylist() {//Tính năng tìm kiếm
-        String searchTerm = searchfield.getText().toLowerCase(); 
-        DefaultListModel model = new DefaultListModel();
-
-        for (int i = 0; i < updateList.size(); i++) {
-            File file = (File) updateList.get(i);
-            String itemName = file.getName();
-
-            if (itemName.toLowerCase().contains(searchTerm)) {
-                model.addElement((i + 1) + " | " + itemName);
+    private void addSearchFieldListener() {
+        searchfield.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updatePlaylist();
             }
-        }
-
-        jPlaylist.setModel(model);
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updatePlaylist();
+            }
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                // Plain text components do not fire these events
+            }
+        });
     }
+    
+    private void updatePlaylist() {
+	    String originalSearchTerm = searchfield.getText().toLowerCase().trim();
+	    DefaultListModel model = new DefaultListModel();
+
+	    for (int i = 0; i < updateList.size(); i++) {
+	        File file = (File) updateList.get(i);
+	        String itemName = file.getName();
+	        String searchTerm = originalSearchTerm.toLowerCase();
+	        int priority = calculatePriority(itemName, searchTerm);
+
+	        if (priority > 0) {
+	            model.addElement(new SearchResult(i, itemName, priority));
+	        }
+	    }
+
+	    model = sortModel(model);
+	    jPlaylist.setModel(model);
+	}
+
+	private int calculatePriority(String itemName, String searchTerm) {
+	    String[] searchTerms = searchTerm.split("\\s+");
+	    int priority = 0;
+
+	    int lastMatchIndex = -1;
+	    for (String term : searchTerms) {
+	        term = term.toLowerCase();
+	        int index = itemName.toLowerCase().indexOf(term, lastMatchIndex + 1);
+	        if (index > lastMatchIndex) {
+	            priority += index - lastMatchIndex;
+	            lastMatchIndex = index;
+	        } else {
+	            return 0; // Not all search terms are matched in order
+	        }
+	    }
+
+	    return priority;
+	}
+
+private DefaultListModel sortModel(DefaultListModel model) {
+    ArrayList<SearchResult> resultList = new ArrayList<>();
+
+    for (int i = 0; i < model.getSize(); i++) {
+        SearchResult result = (SearchResult) model.getElementAt(i);
+        resultList.add(result);
+    }
+    resultList.sort(Comparator.comparingInt(SearchResult::getPriority));
+    DefaultListModel sortedModel = new DefaultListModel();
+    for (SearchResult result : resultList) {
+        sortedModel.addElement(result);
+    }
+
+    return sortedModel;
+}
 
 
     private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
@@ -631,6 +768,29 @@ OpenInfor();
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField1ActionPerformed
+    
+    private static void showBoostWarningDialog(float volume, JSlider volumeSlider) {
+        int choice = JOptionPane.showConfirmDialog(null,
+                "Bạn đang sử dụng Volume Boost, điều này có thể gây hư hại cho tai của bạn và thiết bị nghe nhạc.\nBạn có chắc chắn muốn tiếp tục?",
+                "Cảnh báo Volume Boost", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+
+        if (choice == JOptionPane.YES_OPTION) {
+            setVolume(volume);
+        } else {
+            volumeSlider.setValue(100);
+        }
+    }
+    //Volume control
+    private static void setVolume(float volume) {
+        // Implement your volume control logic here
+        System.out.println("Volume set to: " + volume);
+    }
+    private void adjustVolume(int volume) {
+        // Sử dụng giá trị âm lượng để điều chỉnh âm thanh
+        float volumeLevel = (float) volume / 100.0f;
+        // Áp dụng giá trị âm lượng vào phát nhạc bằng JLayer hoặc bất kỳ thư viện âm thanh nào khác bạn đang sử dụng
+        //player.setVolume(volumeLevel);
+    }
     
     
     /**
@@ -752,13 +912,20 @@ OpenInfor();
     private javax.swing.JTextField jTextField2;
     private javax.swing.JLabel logoutForm;
     private javax.swing.JButton ply;
+    private javax.swing.JProgressBar progressBar;
     private javax.swing.JTextField searchfield;
+    private javax.swing.JButton shuffleB;
     private javax.swing.JButton stop;
+    private javax.swing.JSlider volumnSlider;
     // End of variables declaration//GEN-END:variables
 }
 //Class PlaceHolder
 class PlaceholderTextField extends JTextField implements FocusListener, CaretListener {
-    private String placeholder;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String placeholder;
     private boolean isPlaceholderVisible = true;
     public PlaceholderTextField(String placeholder) {
         this.placeholder = placeholder;
@@ -799,6 +966,31 @@ class PlaceholderTextField extends JTextField implements FocusListener, CaretLis
             isPlaceholderVisible = false;
         }
         repaint();
+    }
+}
+
+class SearchResult {
+    private final int index;
+    private final String itemName;
+    private final int priority;
+
+    public SearchResult(int index, String itemName, int priority) {
+        this.index = index;
+        this.itemName = itemName;
+        this.priority = priority;
+    }
+    
+    public int getIndex() {
+        return index;
+    }
+    
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return index + " | " + itemName;
     }
 }
 
